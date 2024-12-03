bd5f8a7e2afc620748c13ad1692abdc9bce5c584
A7med_Nage7 <a.nageh@bayanatz.com>
2024-12-03 15:03:41 +0200

    create app colors class

---
+++ lib/core/utils/app_colors.dart
@@ -0,0 +1,33 @@
+import 'dart:ui';
+
+class AppColors {
+  // Netural Colour
+  static const Color grayDark = Color(0xff484848);
+  static const Color grayMedium = Color(0xff797979);
+  static const Color grayLight = Color(0xffA9A9A9);
+  static const Color grayLighter = Color(0xffD9D9D9);
+
+  // Primary Color
+  static const Color primaryDark = Color(0xff129575);
+  static const Color primaryDarker = Color(0xff71B1A1);
+  static const Color primaryMedium = Color(0xffAFD3CA);
+  static const Color primaryLight = Color(0xffDBEBE7);
+  static const Color primaryLighter = Color(0xffF6FAF9);
+
+  // secondary Color
+  static const Color secondaryDark = Color(0xffFF9C00);
+  static const Color secondaryDarker = Color(0xffFFA61A);
+  static const Color secondaryMedium = Color(0xffFFBA4D);
+  static const Color secondaryLight = Color(0xffFFCE80);
+  static const Color secondaryLighter = Color(0xffFFE1B3);
+
+  // Rating Color
+  static const Color ratingColor = Color(0xffFFAD30);
+
+  // Warning Color
+  static const Color warningColor = Color(0xffFD3654);
+  static const Color warningColorLight = Color(0xffFFE1E7);
+
+  // Success Color
+  static const Color successColor = Color(0xff31B057);
+}
