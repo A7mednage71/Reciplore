c294cb9cf731cf263c6add8c712ba800c0e0d5cf
A7med_Nage7 <a.nageh@bayanatz.com>
2024-12-03 17:19:19 +0200

    add and handel assets paths with flutter_assets generate

---
+++ lib/core/utils/app_assets.dart
@@ -0,0 +1,9 @@
+// ignore_for_file: prefer_single_quotes
+class AppAssets {
+  AppAssets._();
+  
+  static const String facebookIcon = "assets/facebook_icon.svg";
+
+  static const String googleIcon = "assets/google_icon.svg";
+}
+
