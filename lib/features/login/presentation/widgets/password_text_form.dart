6fc2b8d11bb014bae164086e7288024ed5655948
A7med_Nage7 <a.nageh@bayanatz.com>
2024-12-16 19:20:27 +0200

    add obscureText in password field

--- lib/features/login/presentation/widgets/password_text_form.dart
+++ lib/features/login/presentation/widgets/password_text_form.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:looqma/core/common/widgets/custom_text_field.dart';
+import 'package:looqma/core/utils/app_colors.dart';
 import 'package:looqma/core/utils/app_styles.dart';
 import 'package:looqma/core/utils/my_validator.dart';
 
-class PasswordTextForm extends StatelessWidget {
+class PasswordTextForm extends StatefulWidget {
   const PasswordTextForm({super.key});
 
+  @override
+  State<PasswordTextForm> createState() => _PasswordTextFormState();
+}
+
+class _PasswordTextFormState extends State<PasswordTextForm> {
+  bool obscureText = true;
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -19,6 +26,16 @@ class PasswordTextForm extends StatelessWidget {
           validator: MyValidators.passwordValidator,
           keyboardType: TextInputType.visiblePassword,
           hintText: "Enter Password",
+          obscureText: obscureText,
+          suffixIcon: IconButton(
+            color: AppColors.grayLight,
+            icon: Icon(obscureText ? Icons.visibility : Icons.visibility_off),
+            onPressed: () {
+              setState(() {
+                obscureText = !obscureText;
+              });
+            },
+          ),
         ),
       ],
     );
