69c945232f453fe2b4a228be35ebcf4fc95f74da
A7med_Nage7 <a.nageh@bayanatz.com>
2024-12-17 02:33:51 +0200

    add forget password lottie

--- lib/core/utils/app_assets.dart
+++ lib/core/utils/app_assets.dart
@@ -4,6 +4,8 @@ class AppAssets {
   
   static const String imagesFacebookIcon = "assets/images/facebook_icon.svg";
 
+  static const String imagesForgetPassword = "assets/images/forget_password.json";
+
   static const String imagesGoogleIcon = "assets/images/google_icon.svg";
 
   static const String imagesVerify = "assets/images/verify.json";
