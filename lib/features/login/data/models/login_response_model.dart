4d0189f4df7b335067d6c80bc6f709504a8cf372
A7med_Nage7 <a.nageh@bayanatz.com>
2024-12-17 15:51:05 +0200

    create login response model

---
+++ lib/features/login/data/models/login_response_model.dart
@@ -0,0 +1,11 @@
+import 'package:json_annotation/json_annotation.dart';
+part 'login_response_model.g.dart';
+@JsonSerializable()
+class LoginResponseModel {
+  final String message;
+
+  LoginResponseModel({required this.message});
+
+  factory LoginResponseModel.fromJson(Map<String, dynamic> json) =>
+      _$LoginResponseModelFromJson(json);
+}
