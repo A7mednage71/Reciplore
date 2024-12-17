bf47b1e0b87549c02718057a20b6f21342ddc23a
A7med_Nage7 <a.nageh@bayanatz.com>
2024-12-18 00:16:00 +0200

    add  verification repository instances to di

--- lib/core/di/dependecy_injection.dart
+++ lib/core/di/dependecy_injection.dart
@@ -3,6 +3,9 @@ import 'package:get_it/get_it.dart';
 import 'package:looqma/core/networking/api_service.dart';
 import 'package:looqma/core/networking/dio_factory.dart';
 import 'package:looqma/features/login/data/repos/login_repo.dart';
+import 'package:looqma/features/login/views/cubit/login_cubit.dart';
+import 'package:looqma/features/otp_verify/data/repos/verfication_repo.dart';
+import 'package:looqma/features/otp_verify/views/cubit/verification_cubit.dart';
 
 final getIt = GetIt.instance;
 
@@ -13,5 +16,12 @@ Future<void> setupGetIt() async {
   getIt
     ..registerLazySingleton<ApiService>(() => ApiService(dio))
     // create login repository instance
-    ..registerLazySingleton<LoginRepo>(() => LoginRepo(getIt<ApiService>()));
+    ..registerLazySingleton<LoginRepo>(() => LoginRepo(getIt<ApiService>()))
+    ..registerFactory<LoginCubit>(() => LoginCubit(getIt<LoginRepo>()))
+
+    // create verification repository instance
+    ..registerLazySingleton<VerficationRepo>(
+        () => VerficationRepo(getIt<ApiService>()))
+    ..registerFactory<VerificationCubit>(
+        () => VerificationCubit(getIt<VerficationRepo>()));
 }
