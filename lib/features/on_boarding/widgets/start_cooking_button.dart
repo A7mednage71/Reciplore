173916ea6865c120086a29c4654e09156dc929bb
A7med_Nage7 <a.nageh@bayanatz.com>
2024-12-19 01:07:07 +0200

    create start cooking button

---
+++ lib/features/on_boarding/widgets/start_cooking_button.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:looqma/core/utils/app_colors.dart';
+import 'package:looqma/core/utils/app_styles.dart';
+
+class StartCookingButton extends StatelessWidget {
+  const StartCookingButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {},
+      child: Container(
+        width: 243.w,
+        height: 60.h,
+        decoration: BoxDecoration(
+          color: AppColors.primaryDark,
+          borderRadius: BorderRadius.circular(10.r),
+        ),
+        child: Center(
+            child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Start Cooking",
+              style: AppStyles.normalBoldText.copyWith(color: Colors.white),
+            ),
+            SizedBox(width: 9.w),
+            const Icon(Icons.arrow_forward, color: Colors.white)
+          ],
+        )),
+      ),
+    );
+  }
+}
