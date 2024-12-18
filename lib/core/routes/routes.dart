0bbd56d98ef53616b9a6836dccfb25587db1b1eb
A7med_Nage7 <a.nageh@bayanatz.com>
2024-12-18 02:03:05 +0200

    add route for forget password

--- lib/core/routes/routes.dart
+++ lib/core/routes/routes.dart
@@ -2,5 +2,6 @@ class Routes {
   static const String home = 'Home_Screen';
   static const String login = 'login_Screen';
   static const String verification = 'verification_Screen';
+  static const String forgetPassword = 'forgetPassword_Screen';
   static const String sighnUp = 'sighnUp_Screen';
 }
