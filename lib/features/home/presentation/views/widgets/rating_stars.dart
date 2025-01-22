2a4b79066a5afc5b7475a86bae5c0ed46c5de317
A7med_Nage7 <a.nageh@bayanatz.com>
2025-01-22 19:45:12 +0200

    rating stars widget

---
+++ lib/features/home/presentation/views/widgets/rating_stars.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:looqma/core/utils/app_colors.dart';
+
+class RatingStars extends StatelessWidget {
+  final double rating; // Rating value (e.g., 4.5)
+  final int maxStars; // Total number of stars (e.g., 5)
+
+  const RatingStars({
+    super.key,
+    required this.rating,
+    this.maxStars = 5,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    // Generate the stars
+    List<Widget> stars = List.generate(maxStars, (index) {
+      if (index < rating.floor()) {
+        // Full star
+        return Icon(Icons.star, color: AppColors.ratingColor, size: 20.w);
+      } else if (index < rating && rating % 1 != 0) {
+        // Half star
+        return Icon(Icons.star_half, color: AppColors.ratingColor, size: 20.w);
+      } else {
+        // Empty star
+        return Icon(Icons.star_border, color: Colors.grey, size: 20.w);
+      }
+    });
+
+    return Row(children: stars); // Display stars in a row
+  }
+}
