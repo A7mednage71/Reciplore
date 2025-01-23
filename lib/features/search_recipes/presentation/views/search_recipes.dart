553c72c61b559855438516363f3a3dca21c7e403
A7med_Nage7 <a.nageh@bayanatz.com>
2025-01-24 00:55:52 +0200

    finish search recipes result and search lottie

--- lib/features/search_recipes/presentation/views/search_recipes.dart
+++ lib/features/search_recipes/presentation/views/search_recipes.dart
@@ -1,20 +1,44 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:looqma/core/utils/app_assets.dart';
 import 'package:looqma/features/search_recipes/presentation/views/widgets/search_and_filter_widget.dart';
 import 'package:looqma/features/search_recipes/presentation/views/widgets/search_recipes_appbar.dart';
+import 'package:looqma/features/search_recipes/presentation/views/widgets/search_result.dart';
+import 'package:lottie/lottie.dart';
 
 class SearchRecipes extends StatelessWidget {
   const SearchRecipes({super.key});
 
   @override
+
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: const SearchRecipesAppBar(),
-      body: Column(
-        children: [
-          SizedBox(height: 20.h),
-          const SearchAndFilterWidget(),
-        ],
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 30.w),
+        child: Column(
+          children: [
+            SizedBox(height: 20.h),
+            const SearchAndFilterWidget(),
+            SizedBox(height: 20.h),
+            const Visibility(
+              visible: true,
+              child: Expanded(
+                child: SearchResult(),
+              ),
+            ),
+            Visibility(
+              visible: false,
+              child: Expanded(
+                child: Lottie.asset(
+                  AppAssets.imagesSearchLottie,
+                  height: 250.h,
+                  width: 250.w,
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
