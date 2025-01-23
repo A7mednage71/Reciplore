31ef2ad098c0234d943ee2d55c3a0f25b031eae5
A7med_Nage7 <a.nageh@bayanatz.com>
2025-01-24 00:53:31 +0200

    add search lottie

--- lib/core/utils/app_assets.dart
+++ lib/core/utils/app_assets.dart
@@ -24,6 +24,8 @@ class AppAssets {
 
   static const String imagesSearchIcon = "assets/images/searchIcon.png";
 
+  static const String imagesSearchLottie = "assets/images/searchLottie.json";
+
   static const String imagesSearchSvg = "assets/images/searchSvg.svg";
 
   static const String imagesSearchTest = "assets/images/search_test.png";
