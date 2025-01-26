9184d5f42dfc18c8b42eb622968b556d7de1c4c8
A7med_Nage7 <a.nageh@bayanatz.com>
2025-01-26 02:30:07 +0200

    create saved recipes screen

---
+++ lib/features/saved_recipe/presentation/views/saved_recipe_screen.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:looqma/core/utils/app_styles.dart';
+import 'package:looqma/features/saved_recipe/presentation/views/widgets/saved_recipe_item.dart';
+
+class SavedRecipeScreen extends StatelessWidget {
+  const SavedRecipeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          SizedBox(height: 40.h),
+          Text('Saved recipes', style: AppStyles.mediumBoldText),
+          SizedBox(height: 10.h),
+          Expanded(
+            child: ListView.builder(
+              padding: EdgeInsets.symmetric(horizontal: 30.h),
+              itemCount: 10,
+              itemBuilder: (context, index) {
+                return const SavedRecipeItem();
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
