37af266537851e6f87a9a15817eceb0bd60d6e10
A7med_Nage7 <a.nageh@bayanatz.com>
2025-02-25 02:35:21 +0200

    add Skeletonizer for search item image

--- lib/features/search_recipes/presentation/views/widgets/search_recipe_image.dart
+++ lib/features/search_recipes/presentation/views/widgets/search_recipe_image.dart
@@ -1,8 +1,10 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:looqma/core/utils/app_colors.dart';
 import 'package:looqma/core/utils/app_constants.dart';
 import 'package:looqma/features/home/data/models/get_recipes_response_model.dart';
+import 'package:skeletonizer/skeletonizer.dart';
 
 class SearchRecipeImage extends StatelessWidget {
   const SearchRecipeImage({
@@ -45,15 +47,19 @@ class SearchRecipeImage extends StatelessWidget {
         );
       },
       placeholder: (context, url) {
-        return Container(
-          width: 150.w,
-          height: 150.h,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(10.r),
-            image: const DecorationImage(
-              image: CachedNetworkImageProvider(
-                  AppConstants.defaultRecipeItemImage),
-              fit: BoxFit.cover,
+        return Skeletonizer(
+          enabled: true,
+          child: Container(
+            width: 150.w,
+            height: 150.h,
+            decoration: BoxDecoration(
+              color: AppColors.loadingColor,
+              borderRadius: BorderRadius.circular(10.r),
+              image: const DecorationImage(
+                image: CachedNetworkImageProvider(
+                    AppConstants.defaultRecipeItemImage),
+                fit: BoxFit.cover,
+              ),
             ),
           ),
         );
