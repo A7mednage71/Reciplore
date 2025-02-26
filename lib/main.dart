9f29ae8317707acb033d73b0b63ae0fd3707d152
A7med_Nage7 <a.nageh@bayanatz.com>
2025-02-26 22:47:53 +0200

    app distribution with firebase done

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'dart:developer';
 
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -10,12 +11,16 @@ import 'package:looqma/core/app/internet_checker.dart';
 import 'package:looqma/core/di/dependecy_injection.dart';
 import 'package:looqma/core/services/secure_storage/secure_storage.dart';
 import 'package:looqma/core/services/secure_storage/secure_storage_keys.dart';
+import 'package:looqma/firebase_options.dart';
 import 'package:looqma/my_app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await SystemChrome.setPreferredOrientations(
     [DeviceOrientation.portraitDown, DeviceOrientation.portraitUp],
+  );
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
   );
   await InternetChecker().initialize();
   await ScreenUtil.ensureScreenSize();
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,6 +7,7 @@ import Foundation
 
 import connectivity_plus
 import file_selector_macos
+import firebase_core
 import flutter_inappwebview_macos
 import flutter_secure_storage_macos
 import path_provider_foundation
@@ -15,6 +16,7 @@ import sqflite_darwin
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   ConnectivityPlusPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlusPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   InAppWebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "InAppWebViewFlutterPlugin"))
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
