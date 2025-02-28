2001e9f16f2cf944346e28a22e13b74844431f69
A7med_Nage7 <a.nageh@bayanatz.com>
2025-03-01 01:19:57 +0200

    remove filter button from home screen

--- lib/features/home/presentation/views/widgets/home_search_recipe_section.dart
+++ lib/features/home/presentation/views/widgets/home_search_recipe_section.dart
@@ -3,7 +3,6 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:looqma/core/common/widgets/custom_text_field.dart';
 import 'package:looqma/core/routes/routes.dart';
 import 'package:looqma/core/utils/app_assets.dart';
-import 'package:looqma/features/search_recipes/presentation/views/widgets/filter_button.dart';
 
 class HomeSearchRecipeSection extends StatelessWidget {
   const HomeSearchRecipeSection({super.key});
@@ -12,32 +11,26 @@ class HomeSearchRecipeSection extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.symmetric(horizontal: 30.w),
-      child: Row(
-        children: [
-          Expanded(
-            child: CustomTextField(
-              controller: TextEditingController(),
-              validator: null,
-              onChanged: (value) => null,
-              contentPadding:
-                  EdgeInsets.symmetric(horizontal: 20.w, vertical: 10.h),
-              keyboardType: TextInputType.text,
-              prefixIcon: Image.asset(
-                AppAssets.imagesSearchIcon,
-                height: 20.h,
-                width: 20.w,
-              ),
-              hintText: "Search recipe",
-              readOnly: true,
-              onTap: () {
-                Navigator.of(context, rootNavigator: true)
-                    .pushNamed(Routes.searchScreen);
-              },
-            ),
+      child: Expanded(
+        child: CustomTextField(
+          controller: TextEditingController(),
+          validator: null,
+          onChanged: (value) => null,
+          contentPadding:
+              EdgeInsets.symmetric(horizontal: 20.w, vertical: 10.h),
+          keyboardType: TextInputType.text,
+          prefixIcon: Image.asset(
+            AppAssets.imagesSearchIcon,
+            height: 20.h,
+            width: 20.w,
           ),
-          SizedBox(width: 20.w),
-          const FilterButton(enabled: false),
-        ],
+          hintText: "Search recipe",
+          readOnly: true,
+          onTap: () {
+            Navigator.of(context, rootNavigator: true)
+                .pushNamed(Routes.searchScreen);
+          },
+        ),
       ),
     );
   }
