9ae124a7b38fea17567aa039ee4318220f037a15
A7med_Nage7 <a.nageh@bayanatz.com>
2025-03-10 20:00:20 +0200

    add refresh indecator in SavedRecipesListView

--- lib/features/saved_recipe/presentation/views/widgets/saved_recipes_list_view.dart
+++ lib/features/saved_recipe/presentation/views/widgets/saved_recipes_list_view.dart
@@ -1,7 +1,9 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:looqma/core/routes/routes.dart';
 import 'package:looqma/features/home/data/models/get_recipes_response_model.dart';
+import 'package:looqma/features/saved_recipe/presentation/cubit/get_saved_recipes/get_saved_recipes_cubit.dart';
 import 'package:looqma/features/saved_recipe/presentation/views/widgets/saved_recipe_item.dart';
 
 class SavedRecipesListView extends StatelessWidget {
@@ -14,22 +16,27 @@ class SavedRecipesListView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Expanded(
-      child: ListView.builder(
-        padding: EdgeInsets.symmetric(horizontal: 30.h, vertical: 15.h),
-        itemCount: savedRecipes.length,
-        itemBuilder: (context, index) {
-          return GestureDetector(
-            onTap: () {
-              Navigator.of(context, rootNavigator: true).pushNamed(
-                  Routes.showRecipeDetails,
-                  arguments: savedRecipes[index]);
-            },
-            child: Padding(
-              padding: EdgeInsets.only(top: index == 0 ? 0 : 15.h),
-              child: SavedRecipeItem(recipeModel: savedRecipes[index]),
-            ),
-          );
+      child: RefreshIndicator(
+        onRefresh: () async {
+          context.read<GetSavedRecipesCubit>().getSavedRecipes();
         },
+        child: ListView.builder(
+          padding: EdgeInsets.symmetric(horizontal: 30.h, vertical: 15.h),
+          itemCount: savedRecipes.length,
+          itemBuilder: (context, index) {
+            return GestureDetector(
+              onTap: () {
+                Navigator.of(context, rootNavigator: true).pushNamed(
+                    Routes.showRecipeDetails,
+                    arguments: savedRecipes[index]);
+              },
+              child: Padding(
+                padding: EdgeInsets.only(top: index == 0 ? 0 : 15.h),
+                child: SavedRecipeItem(recipeModel: savedRecipes[index]),
+              ),
+            );
+          },
+        ),
       ),
     );
   }
