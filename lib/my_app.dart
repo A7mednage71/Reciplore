8601101464ff60c243984537b56df1f9455d61ed
A7med_Nage7 <a.nageh@bayanatz.com>
2025-03-11 02:53:48 +0200

    provide RecipeSaveToggleCubit above material

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:looqma/core/app/constants.dart';
 import 'package:looqma/core/app/internet_checker.dart';
+import 'package:looqma/core/common/recipe_save_toggle/cubit/recipe_save_toggle_cubit.dart';
+import 'package:looqma/core/di/dependecy_injection.dart';
 import 'package:looqma/core/routes/app_router.dart';
 import 'package:looqma/core/routes/routes.dart';
 
@@ -15,29 +18,32 @@ class MyApp extends StatelessWidget {
       designSize: const Size(375, 812),
       minTextAdapt: true,
       builder: (context, child) {
-        return MaterialApp(
-          title: 'Looqma',
-          debugShowCheckedModeBanner: false,
-          onGenerateRoute: AppRouter.getRoute,
-          initialRoute:
-              isUserlogged ? Routes.navBarScreensSwitcher : Routes.onBording,
-          navigatorKey: navigatorKey,
-          theme: ThemeData(
-            colorScheme: ColorScheme.fromSeed(seedColor: Colors.white),
-            useMaterial3: true,
+        return BlocProvider(
+          create: (context) => getIt<RecipeSaveToggleCubit>(),
+          child: MaterialApp(
+            title: 'Looqma',
+            debugShowCheckedModeBanner: false,
+            onGenerateRoute: AppRouter.getRoute,
+            initialRoute:
+                isUserlogged ? Routes.navBarScreensSwitcher : Routes.onBording,
+            navigatorKey: navigatorKey,
+            theme: ThemeData(
+              colorScheme: ColorScheme.fromSeed(seedColor: Colors.white),
+              useMaterial3: true,
+            ),
+            builder: (context, child) {
+              return StreamBuilder<bool>(
+                stream: InternetChecker().onConnectionChange,
+                builder: (context, snapshot) {
+                  // final isConnected = snapshot.data ?? false;
+                  // if (!isConnected) {
+                  //   return const NoInternetConnection();
+                  // }
+                  return child!;
+                },
+              );
+            },
           ),
-          builder: (context, child) {
-            return StreamBuilder<bool>(
-              stream: InternetChecker().onConnectionChange,
-              builder: (context, snapshot) {
-                // final isConnected = snapshot.data ?? false;
-                // if (!isConnected) {
-                //   return const NoInternetConnection();
-                // }
-                return child!;
-              },
-            );
-          },
         );
       },
     );
