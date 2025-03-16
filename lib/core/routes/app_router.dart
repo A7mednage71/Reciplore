f2601da1863ca8e9e2ec8eeae8d0cf93d67b5e56
A7med_Nage7 <a.nageh@bayanatz.com>
2025-03-16 03:42:15 +0200

    create routes to all ingredients screen

--- lib/core/routes/app_router.dart
+++ lib/core/routes/app_router.dart
@@ -4,6 +4,7 @@ import 'package:looqma/core/common/screens/no_internet_connection.dart';
 import 'package:looqma/core/common/screens/no_route_screen.dart';
 import 'package:looqma/core/di/dependecy_injection.dart';
 import 'package:looqma/core/routes/routes.dart';
+import 'package:looqma/features/all_ingredients/presentation/views/all_ingredients_screen.dart';
 import 'package:looqma/features/cart/presentation/views/cart_screen.dart';
 import 'package:looqma/features/category_recipes/presentation/views/category_recipes.dart';
 import 'package:looqma/features/change_password/presentation/cubit/change_password/change_password_cubit.dart';
@@ -128,6 +129,10 @@ class AppRouter {
       case Routes.cart:
         return MaterialPageRoute(
           builder: (context) => const CartScreen(),
+        );
+      case Routes.allIngredients:
+        return MaterialPageRoute(
+          builder: (context) => const AllIngredientsScreen(),
         );
       case Routes.profile:
         return MaterialPageRoute(
