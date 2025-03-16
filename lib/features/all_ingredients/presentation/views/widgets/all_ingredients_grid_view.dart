76b6ea22451d0b412689a6a39647fe964349d78c
A7med_Nage7 <a.nageh@bayanatz.com>
2025-03-16 03:41:44 +0200

    create all ingredients grid view

---
+++ lib/features/all_ingredients/presentation/views/widgets/all_ingredients_grid_view.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:looqma/core/routes/routes.dart';
+import 'package:looqma/features/home_market/presentation/views/widgets/market_ingredient_item.dart';
+
+class AllIngredientsGridView extends StatelessWidget {
+  const AllIngredientsGridView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: GridView.builder(
+        itemCount: 10,
+        padding: EdgeInsets.all(20.w),
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          mainAxisSpacing: 10.h,
+          crossAxisSpacing: 15.w,
+          childAspectRatio: 0.65,
+        ),
+        itemBuilder: (context, index) {
+          return GestureDetector(
+              onTap: () {
+                Navigator.of(context, rootNavigator: true)
+                    .pushNamed(Routes.marketIngredientsDetails);
+              },
+              child: const MarketIngredientItem());
+        },
+      ),
+    );
+  }
+}
