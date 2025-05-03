61b26ca2570598849ffd2eb4941936ae5d7d8e9a
A7med_Nage7 <a.nageh@bayanatz.com>
2025-05-04 02:55:06 +0300

    - make chat service run locally - create special dio for local requests with 60 sec for timeout

---
+++ lib/core/networking/api_local_service.dart
@@ -0,0 +1,18 @@
+import 'package:dio/dio.dart';
+import 'package:looqma/core/networking/api_constants.dart';
+import 'package:looqma/features/chat_bot/data/models/chat_bot_request_model.dart';
+import 'package:looqma/features/chat_bot/data/models/chat_bot_response_model.dart';
+import 'package:retrofit/retrofit.dart';
+
+part 'api_local_service.g.dart';
+
+@RestApi(baseUrl: ApiConstants.baseUrlEmulator)
+abstract class ApiLocalService {
+  factory ApiLocalService(Dio dio) => _ApiLocalService(dio);
+
+  // AI services
+  @POST(ApiConstants.chatBot)
+  Future<ChatBotResponseModel> chatBotRecommendation(
+    @Body() ChatBotRequestModel body,
+  );
+}
