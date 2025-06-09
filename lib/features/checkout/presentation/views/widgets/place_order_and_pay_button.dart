3b77854aec044c50e97b050d2dffea719eb400c3
A7med_Nage7 <a.nageh@bayanatz.com>
2025-06-09 04:49:12 +0300

    handel logic and state in place order button

---
+++ lib/features/checkout/presentation/views/widgets/place_order_and_pay_button.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:looqma/core/common/widgets/show_toast.dart';
+import 'package:looqma/core/utils/app_colors.dart';
+import 'package:looqma/core/utils/app_styles.dart';
+import 'package:looqma/features/checkout/presentation/cubit/checkout/checkout_cubit.dart';
+import 'package:looqma/features/checkout/presentation/views/widgets/loading_payment_session_link_dialog.dart';
+
+class PlaceOrderAndPayButton extends StatelessWidget {
+  const PlaceOrderAndPayButton({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final checkoutCubit = context.read<CheckoutCubit>();
+    return BlocConsumer<CheckoutCubit, CheckoutState>(
+      listenWhen: (previous, current) =>
+          previous.placeOrderStatus != current.placeOrderStatus &&
+          (current.placeOrderStatus == PlaceOrderStatus.failure ||
+              current.placeOrderStatus == PlaceOrderStatus.success),
+      listener: (context, state) {
+        if (state.placeOrderStatus == PlaceOrderStatus.failure) {
+          ShowToast.showFailureToast(state.placeOrderMessage ?? '');
+        }
+        if (state.placeOrderStatus == PlaceOrderStatus.success) {
+          ShowToast.showSuccessToast(state.placeOrderMessage ?? '');
+
+          String orderId = state.placeOrderResponse!.order.id;
+
+          showDialog(
+              context: context,
+              builder: (context) => BlocProvider.value(
+                    value: checkoutCubit,
+                    child: LoadingPaymentSessionLinkDialog(orderId: orderId),
+                  ));
+        }
+      },
+      buildWhen: (previous, current) =>
+          previous.placeOrderStatus != current.placeOrderStatus,
+      builder: (context, state) {
+        if (state.placeOrderStatus == PlaceOrderStatus.loading) {
+          return Container(
+            width: double.infinity,
+            height: 60.h,
+            decoration: BoxDecoration(
+              color: AppColors.primaryDark,
+              borderRadius: BorderRadius.circular(10.r),
+            ),
+            child: Center(
+              child: SpinKitFadingCircle(color: AppColors.white, size: 40.r),
+            ),
+          );
+        } else {
+          return InkWell(
+            onTap: () async {
+              final checkout = context.read<CheckoutCubit>();
+              if (!checkout.contactNumberFormKey.currentState!.validate()) {
+                return;
+              }
+              if (checkout.state.paymentMethod == null) {
+                ShowToast.showFailureToast('Please select payment method');
+                return;
+              }
+              if (checkout.state.shippingAddressID == null) {
+                ShowToast.showFailureToast('Please select shipping address');
+                return;
+              }
+              await checkout.placeOrder();
+            },
+            child: Container(
+              width: double.infinity,
+              height: 60.h,
+              decoration: BoxDecoration(
+                color: AppColors.primaryDark,
+                borderRadius: BorderRadius.circular(10.r),
+              ),
+              child: Center(
+                child: Text(
+                  "Place Order & Pay",
+                  style: AppStyles.normalBoldWhiteText,
+                ),
+              ),
+            ),
+          );
+        }
+      },
+    );
+  }
+}
