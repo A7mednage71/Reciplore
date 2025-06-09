9e690ce7c53a2d187f985cb8a43a8b57d91a612b
A7med_Nage7 <a.nageh@bayanatz.com>
2025-06-09 20:49:55 +0300

    add refresh indicator for profile and saved recipes

--- lib/features/my_profile/presentation/views/screens/my_profile.dart
+++ lib/features/my_profile/presentation/views/screens/my_profile.dart
@@ -4,6 +4,7 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:iconly/iconly.dart';
 import 'package:looqma/core/common/widgets/show_toast.dart';
 import 'package:looqma/core/routes/routes.dart';
+import 'package:looqma/core/utils/app_colors.dart';
 import 'package:looqma/core/utils/app_styles.dart';
 import 'package:looqma/features/my_profile/presentation/cubit/get_user_profile/get_user_profile_cubit.dart';
 import 'package:looqma/features/my_profile/presentation/views/widgets/my_profile/join_community.dart';
@@ -20,46 +21,53 @@ class MyProfile extends StatelessWidget {
     return Scaffold(
       body: Padding(
         padding: EdgeInsets.symmetric(horizontal: 30.w),
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              SizedBox(height: 40.h),
-              Align(
-                alignment: Alignment.topLeft,
-                child: Text(
-                  'My Profile',
-                  style: AppStyles.largeBoldText,
+        child: RefreshIndicator(
+          color: AppColors.primaryDarker,
+          onRefresh: () async {
+            await getUserProfileCubit.getUserProfile();
+          },
+          child: SingleChildScrollView(
+            physics: const AlwaysScrollableScrollPhysics(),
+            child: Column(
+              children: [
+                SizedBox(height: 40.h),
+                Align(
+                  alignment: Alignment.topLeft,
+                  child: Text(
+                    'My Profile',
+                    style: AppStyles.largeBoldText,
+                  ),
                 ),
-              ),
-              SizedBox(height: 20.h),
-              const ShowUserDataBlocBuilder(),
-              SizedBox(height: 20.h),
-              ProfileListTileItem(
-                title: "Update Info",
-                icon: IconlyLight.edit_square,
-                onTap: () async {
-                  await updateUserInfo(getUserProfileCubit, context);
-                },
-              ),
-              ProfileListTileItem(
-                title: "Change Password",
-                icon: Icons.lock_outline,
-                onTap: () {
-                  Navigator.of(context, rootNavigator: true)
-                      .pushNamed(Routes.changePassword);
-                },
-              ),
-              ProfileListTileItem(
-                title: "About",
-                icon: Icons.info_outline_rounded,
-                onTap: () {},
-              ),
-              SizedBox(height: 20.h),
-              const LogoutButton(),
-              SizedBox(height: 10.h),
-              const JoinCommunityWidget(),
-              SizedBox(height: 40.h),
-            ],
+                SizedBox(height: 20.h),
+                const ShowUserDataBlocBuilder(),
+                SizedBox(height: 20.h),
+                ProfileListTileItem(
+                  title: "Update Info",
+                  icon: IconlyLight.edit_square,
+                  onTap: () async {
+                    await updateUserInfo(getUserProfileCubit, context);
+                  },
+                ),
+                ProfileListTileItem(
+                  title: "Change Password",
+                  icon: Icons.lock_outline,
+                  onTap: () {
+                    Navigator.of(context, rootNavigator: true)
+                        .pushNamed(Routes.changePassword);
+                  },
+                ),
+                ProfileListTileItem(
+                  title: "About",
+                  icon: Icons.info_outline_rounded,
+                  onTap: () {},
+                ),
+                SizedBox(height: 20.h),
+                const LogoutButton(),
+                SizedBox(height: 10.h),
+                const JoinCommunityWidget(),
+                SizedBox(height: 40.h),
+              ],
+            ),
           ),
         ),
       ),
