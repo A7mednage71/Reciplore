3b5751752848962a0082355d91e8a54c8f982706
A7med_Nage7 <a.nageh@bayanatz.com>
2025-06-09 04:42:21 +0300

    create checkout repo for all services in checkout screen

--- lib/features/checkout/data/repos/checkout_repo.dart
+++ lib/features/checkout/data/repos/checkout_repo.dart
@@ -6,6 +6,11 @@ import 'package:looqma/core/networking/response_message_model.dart';
 import 'package:looqma/features/checkout/data/models/add_and_update_address_request_model.dart';
 import 'package:looqma/features/checkout/data/models/add_and_update_address_response_model.dart';
 import 'package:looqma/features/checkout/data/models/cart_overview_response_model.dart';
+import 'package:looqma/features/checkout/data/models/check_coupon_request_model.dart';
+import 'package:looqma/features/checkout/data/models/check_coupon_response_model.dart';
+import 'package:looqma/features/checkout/data/models/checkout_session_response_model.dart';
+import 'package:looqma/features/checkout/data/models/place_order_request_model.dart';
+import 'package:looqma/features/checkout/data/models/place_order_response_model.dart';
 
 class CheckoutRepo {
   final ApiService _apiService;
@@ -15,6 +20,45 @@ class CheckoutRepo {
   Future<ApiResult<CartOverviewResponseModel>> getCartOverview() async {
     try {
       final result = await _apiService.getCartOverview();
+      return ApiResult.success(result);
+    } catch (e) {
+      if (e is DioException) {
+        return ApiResult.failure(ServerFailure.fromDioError(e));
+      }
+      return ApiResult.failure(ApiError(e.toString()));
+    }
+  }
+
+  Future<ApiResult<CheckCouponResponseModel>> chechCoupon(
+      {required CheckCouponRequestModel checkCouponRequestModel}) async {
+    try {
+      final result = await _apiService.checkCoupon(checkCouponRequestModel);
+      return ApiResult.success(result);
+    } catch (e) {
+      if (e is DioException) {
+        return ApiResult.failure(ServerFailure.fromDioError(e));
+      }
+      return ApiResult.failure(ApiError(e.toString()));
+    }
+  }
+
+  Future<ApiResult<PlaceOrderResponseModel>> placeOrder(
+      {required PlaceOrderRequestModel placeOrderRequestModel}) async {
+    try {
+      final result = await _apiService.placeOrder(placeOrderRequestModel);
+      return ApiResult.success(result);
+    } catch (e) {
+      if (e is DioException) {
+        return ApiResult.failure(ServerFailure.fromDioError(e));
+      }
+      return ApiResult.failure(ApiError(e.toString()));
+    }
+  }
+
+  Future<ApiResult<CheckoutSessionResponseModel>> payWithStripe(
+      {required String orderId}) async {
+    try {
+      final result = await _apiService.payWithStripe(orderId);
       return ApiResult.success(result);
     } catch (e) {
       if (e is DioException) {
